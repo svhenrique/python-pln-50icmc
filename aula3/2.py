cef228081e782023fe91e8f46b68b944bd4c03c8
svhenrique <silviohenrique1234@hotmail.com>
2021-10-03 00:17:54 -0300

    changed q2 text

--- aula3/2.py
+++ aula3/2.py
@@ -1,6 +1,5 @@
 """
-Crie  três  variáveis  e  atribua  os  valores  a  seguir:  a=1,  b=5.9  e  c=‘teste’.  A 
-partir disso, retorne o tipo de cada uma das variáveis. 
+Troque o valor da variável a por ‘1’ e verifique se o tipo da variável mudou.
 """
 
 a = 1
